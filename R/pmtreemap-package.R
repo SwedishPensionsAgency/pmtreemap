e5da86b360098f3ce323a614deb1906fb881b36e
LCHansson <love.hansson@pensionsmyndigheten.se>
2015-04-01 16:00:37 +0200

    General code cleanup

---
+++ R/pmtreemap-package.R
@@ -0,0 +1,7 @@
+#' pmtreemap
+#'
+#' The pmtreemap package is a htmlwidget for D3 based treemaps.
+#'
+#' @docType package
+#' @name pmtreemap
+NULL
--- R/treemap.R
+++ R/treemap.R
@@ -1,13 +1,11 @@
-#' <Add Title>
+#' treemap
 #'
-#' <Add Description>
+#' htmlwidget wrapper function for D3 treemaps
 #'
 #' @import htmlwidgets
 #'
 #' @export
 treemap <- function(data, width = NULL, height = NULL) {
-  print(paste("treemap, ", "width = ",width))
-  #print(paste("treemap, message: ", toString(message)))
   
   # forward options using x
   x = list(
@@ -21,7 +19,6 @@ treemap <- function(data, width = NULL, height = NULL) {
     x = x,
     width = width,
     height = height,
-    #htmlwidgets::sizingPolicy(padding = 0, browser.fill = TRUE), # Tillagd
     package = 'pmwidgets'
   )
 }
@@ -30,17 +27,13 @@ treemap <- function(data, width = NULL, height = NULL) {
 #'
 #' @export
 treemapOutput <- function(outputId, width = '600px', height = '600px'){
-#treemapOutput <- function(outputId, width, height){
-  print(paste("treemapOutput, ","width = ",width))
   shinyWidgetOutput(outputId, 'pmtreemap', width = width, height = height, package = 'pmtreemap')
-  #shinyWidgetOutput(outputId, 'treemap', width, height)
 }
 
 #' Widget render function for use in Shiny
 #'
 #' @export
 renderTreemap <- function(expr, env = parent.frame(), quoted = FALSE) {
-  print("renderTreemap")
   if (!quoted) { expr <- substitute(expr) } # force quoted
   shinyRenderWidget(expr, treemapOutput, env, quoted = TRUE)
 }
